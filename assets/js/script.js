fd6afe20110d76a4ebafb1e42e2adc5d65681cb7
Shawn <shawnmprasad@gmail.com>
2022-12-18 17:45:20 -1000

    fixed javascript, creates new divs to task list

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,44 +1,26 @@
-
 window.addEventListener('load', () => {
     const form = document.querySelector('#new-task-form'); //keep
-    const input = document.querySelector('.form-control');
+    const inputs = document.querySelectorAll('.form-control');
     const list_el = document.querySelector('#tasks');
-
-
-
     form.addEventListener('submit', (e) => {
         e.preventDefault(); // prevents refreshing the page
-
-        const task = input.value;
-        
+      
+          inputs.forEach((input) => {
+            console.log(input.value);
+    
+            const task = input.value;
+            const task_content_el = document.createElement("div");
+    
+            task_content_el.classList.add('content');
+            task_content_el.innerText = task; //set inner value to our task
+    
+            list_el.appendChild(task_content_el);
+        });
+      
         // if (!task) {
         //     alert('fill out tasks');
         //     return;
-        // } 
-
-        const task_el = document.createElement('div');
-        task_el.classList.add('task');
-
-        const task_content_el = document.createElement("div");
-        task_content_el.classList.add('content');
-
-        task_content_el.innerText = task; //set inner value to our task
-
-        task_el.appendChild(task_content_el);
-
-        list_el.appendChild(task_el);
-
-        // inputs.forEach((input) => {
-        //     inputs.addEventListener('click', () => {
-        //       console.log("forEach worked");
-        //       task_el.appendChild(task_content_el);
-        //     });
+        // }
+        
           });
     });
-
-
-
-
-
-
-
