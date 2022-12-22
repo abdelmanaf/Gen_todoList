f5046b20699eac64b311da910b6f83392371b000
abdelmanaf <56102230+abdelmanaf@users.noreply.github.com>
2022-12-21 21:21:27 -0500

    fix all problems

---
+++ assets/js/script.js
@@ -0,0 +1,43 @@
+window.addEventListener('load', () => {
+  const form = document.querySelector('#new-task-form'); 
+  const list_el = document.querySelector('#task');
+  const textName = document.querySelector('#textName')
+  const textDescription = document.querySelector('#textDescription')
+  const textAssign = document.querySelector('#textAssign')
+  const dueDate = document.querySelector('#dueDate')
+  const status = document.getElementById("status")
+  
+  form.addEventListener('submit', (e) => {
+      e.preventDefault(); // prevents refreshing the page
+
+      let taskName = textName.value;
+      let taskDescription = textDescription.value;
+      let taskAssign = textAssign.value;
+      let taskDueDate = dueDate.value;
+    
+      if (!taskName || !taskDescription || !taskAssign || !taskDueDate) {
+          alert('fill out tasks');
+          return;
+      } else {
+        const task_content_el = document.createElement("div");
+  
+          task_content_el.innerHTML = 
+          `<div>
+            Task Name: ${taskName} <br />
+            Task Description: ${taskDescription} <br />
+            Assign to: ${taskAssign} <br />
+            Due date: ${taskDueDate}
+          </div>` 
+          task_content_el.classList.add('tasks');
+          list_el.appendChild(task_content_el);
+      }
+
+      function validateForm(){
+        if (status.value === ""){
+          alert('Please enter task description');
+        }
+      }
+      
+       
+  });
+});
